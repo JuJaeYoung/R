dc5fa4b4cf850cefdeed53f700f653deec9ad5c7
JuJaeYoung <65886205+JuJaeYoung@users.noreply.github.com>
2022-09-26 09:27:47 +0900

    Add files via upload

---
+++ Ex20220923_2.R
@@ -0,0 +1,5 @@
+# shopUser, shopProduct, shopSale 테이블 생성
+# shopUser : uId(문자열10, primary key), uName(문자열20), uAge(정수)
+# shopProduct : pCode(정수, primary key), pName(문자열20), pPrice(정수)
+# shopSale : uId, pCode, sCount(정수)
+
